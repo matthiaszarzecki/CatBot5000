c864eff99c3ab84e7f4de009f7807b6f2c5c12be
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2019-06-10 23:37:53 +0200

    Add bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'insert_key_here',
+  consumer_secret:      'insert_key_here',
+  access_token:         'insert_key_here',
+  access_token_secret:  'insert_key_here'
+}
